99ca99a3b4615f49c92fbbde258b42ee924f681c
mariadjhernandezcas-glitch <mariadjhernandezcas@gmail.com>
2026-08-25 12:09:33 -0500

    Remove duplicated file from main

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { listTickets } from "@/lib/tickets";
-import { KanbanBoard } from "@/components/KanbanBoard";
-
-export const dynamic = "force-dynamic";
-
-export default function HomePage() {
-  const tickets = listTickets();
-  return <KanbanBoard tickets={tickets} />;
-}
